26506f5502b58be8859f213fe5c6b899cc37e22f
UNI-FEC <UNI-FEC@DESKTOP-57SG22O>
2020-12-04 10:52:51 -0600

    Proyecto version 3.1

--- ProyectoFinal/src/proyectofinal/Conexion.java
+++ ProyectoFinal/src/proyectofinal/Conexion.java
@@ -4,39 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- * @author @soy_megh
- */
-public class Conexion {
- private static final String SERVIDOR = "MARLIE\\SQLEXPRESS";
-    private static final String USUARIO = "sa";
-    private static final String PW = "123";
-    private static final String NOMBREBD = "Proyecto_Final";
-    private static final String PUERTO = "1433";
-    private static final String DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    
-    public Connection obtenerConexion(){
-        try{
-            String conexionUrl = "jdbc:sqlserver://" + SERVIDOR +": " + PUERTO + "; Databasename= " + NOMBREBD +"; user= " + USUARIO + "; password = " + PW + ";";
-            Class.forName(DRIVER);
-            
-            return (DriverManager.getConnection(conexionUrl));
-        
-        }catch(ClassNotFoundException | SQLException ex){
-            ex.printStackTrace();
-        }
-        
-        return null;
+public class Conexion
+{
+    private Connection cn=null;
+    public Conexion() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException
+    {
+      Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+      cn = DriverManager.getConnection("jdbc:sqlserver://165.98.12.158:1432;databaseName=Prueba;user=poo2;password=poo2");
     }
-    
-    
-    public void close(Connection conn){
-        try{
-           conn.close();
-        }
-        catch(Exception ex){
-            ex.printStackTrace();
-        }
+    public Connection getConexion()
+    {
+        return cn;
     }
-    
+    public void cerrar() throws SQLException
+    {
+        cn.close();
+    }
 }
